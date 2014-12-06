f04a253d2ff04a59ee1855c0704361220d779b6a
Mario <mpontice@gmail.com>
2014-12-06 12:02:07 -0800

    create TodoApp component and app.init function

--- empty-projects/react-todo/js/app.jsx
+++ empty-projects/react-todo/js/app.jsx
@@ -4,4 +4,19 @@ var app = app || {};
 
 (function() {
   'use strict';
+
+  app.init = function() {
+    // first, we're using the line below to take the TodoApp function from components and bring it into this scope so that it can be available for renderComponent
+    var TodoApp = app.components.TodoApp;
+    // Next, we're rendering that component in the dom
+    // First, we're calling React.renderComponent, which takes two arguments: the thing you want rendered, and where in index.html it should be rendered
+    // We're using jsx syntax to insert the TodoApp function in a dom-element-like wrapper,
+    // and then we're using getElementById to identify and append it to the section with id 'app' in index.html
+    React.renderComponent (
+      <TodoApp />,
+      document.getElementById('app')
+    );
+  };
+
+  app.init();
 })();
--- empty-projects/react-todo/js/components.jsx
+++ empty-projects/react-todo/js/components.jsx
@@ -6,5 +6,14 @@ app.components = app.components || {};
 (function() {
   'use strict';
 
-  
+  var TodoApp = app.components.TodoApp = React.createClass({
+    render: function() {
+      // jsx can only have one root element
+      return (
+        <div className="outer-container">
+          <p>interesting text goes here</p>
+        </div>
+      );
+    }
+  });
 })();
